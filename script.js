13aaec71169daacf8e4792aceee9e23cd7a53ecc
Valentin Launay <launay.val@hotmail.fr>
2025-06-17 20:45:23 +0200

    site v2

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+
+ function toggleModules(button) {
+  const list = button.nextElementSibling;
+  list.classList.toggle('hidden');
+  button.textContent = list.classList.contains('hidden')
+    ? 'Voir les modules associés'
+    : 'Masquer les modules';
+}
+
+
+
